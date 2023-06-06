b1f88bec91cafc62e6ff3003efb1b4e329b169eb
Rodrigo <rodrigorodgrt002@gmail.com>
2023-06-06 20:55:17 -0300

    :fire:

---
+++ pages/cursos/form.js
@@ -0,0 +1,72 @@
+import Pagina from '@/components/Pagina'
+import React, { useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { BsCheckLg } from 'react-icons/bs'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
+
+const form = () => {
+
+    const { push } = useRouter()
+    const { register, handleSubmit, formState: {errors} } = useForm()
+
+    function salvar(dados) {
+        const cursos = JSON.parse(window.localStorage.getItem('cursos')) || []
+        cursos.push(dados)
+        window.localStorage.setItem('cursos', JSON.stringify(cursos))
+        push('/cursos')
+    }
+
+        const validatorNome = {
+            required:'o campo é obrigatorio',
+            minLength: {
+                value: 3,
+                message:'a quantidade minima de caracter é 3',
+            },
+            maxLength:{
+                 value: 10,
+                message:'a quantidade màxima de caracter é 10',
+            }
+            
+
+        }
+    return (
+        <Pagina titulo="Curso">
+            <Form>
+                <Form.Group className="mb-3" controlId="nome">
+                    <Form.Label>Nome: </Form.Label>
+                    <Form.Control type="text" {...register('nome', validatorNome)} />
+                    {
+                        errors.nome &&
+                        <small>{errors.nome.message}</small>
+                    }
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="duracao">
+                    <Form.Label>Duração: </Form.Label>
+                    <Form.Control type="text" {...register('duracao')} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="modalidade">
+                    <Form.Label>Modalidade: </Form.Label>
+                    <Form.Control type="text" {...register('modalidade')} />
+                </Form.Group>
+
+                <div className='text-center'>
+                    <Button variant="success" onClick={handleSubmit(salvar)}>
+                        <BsCheckLg className="me-2" />
+                        Salvar
+                    </Button>
+                    <Link className="ms-2 btn btn-danger" href="/cursos">
+                        <AiOutlineArrowLeft className="me-2" />
+                        Voltar
+                    </Link>
+                </div>
+            </Form>
+        </Pagina>
+    )
+}
+
+export default form
